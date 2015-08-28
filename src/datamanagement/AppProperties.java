f0ca3d2185aa9853ddc41ff5610960419c64d316
quachvu <nomatterhow21@yahoo.com>
2015-08-28 19:03:03 +1000

    first commit
    first commit

---
+++ src/datamanagement/AppProperties.java
@@ -0,0 +1,33 @@
+package datamanagement;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * This is Properties file of the App
+ * Edited by VuQuach
+ * */
+public class AppProperties {
+	private static AppProperties self = null;
+	private Properties properties;
+
+	public static AppProperties getInstance() {
+		if (self == null) {
+			self = new AppProperties();
+		}
+		return self;
+	}
+
+	private AppProperties() {
+		properties = new Properties();
+		try {
+			properties.load(new FileInputStream("Properties.prop"));
+		} catch (IOException e) {
+			throw new RuntimeException("Could not read property file");
+		}
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+}
